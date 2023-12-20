a681155cb20b468b155f493260f3cd50dab40377
James Ryan <jryan@letsgetchecked.com>
2023-12-20 20:38:08 +0000

    EXTRA_MESSAGE and sendMessage body

--- app/src/main/java/com/dcu/ass2/MainActivity.kt
+++ app/src/main/java/com/dcu/ass2/MainActivity.kt
@@ -1,16 +1,27 @@
 package com.dcu.ass2
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
+//  Kotlin companion object acts as static members. @JvmField declares val as public final
+//  Original Java: public final static String EXTRA_MESSAGE = "com.dcu.ass2.MESSAGE";
+    companion object {
+        @JvmField val EXTRA_MESSAGE: String = "com.dcu.ass2.MESSAGE"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
     fun sendMessage(view: View) {
-
+        val intent = Intent(this, DisplayMessageActivity::class.java)
+        val editText = findViewById<View>(R.id.edit_message) as EditText
+        val message = editText.text.toString()
+        intent.putExtra(EXTRA_MESSAGE, message)
     }
 }
