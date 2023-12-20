c95f9f82043cdcd2c5e1fd219d40bb614c8c9421
James Ryan <jryan@letsgetchecked.com>
2023-12-20 21:10:33 +0000

    displayMessageActivity with message

--- app/src/main/java/com/dcu/ass2/DisplayMessageActivity.kt
+++ app/src/main/java/com/dcu/ass2/DisplayMessageActivity.kt
@@ -1,11 +1,37 @@
 package com.dcu.ass2
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.TextView
+import androidx.core.app.NavUtils
 
-class DisplayMessageActivity : AppCompatActivity() {
+
+class DisplayMessageActivity : Activity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_display_message)
+
+        // Get the message from the intent
+        val intent = intent
+        val message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE)
+
+        // Create the text view
+        val textView = TextView(this)
+        textView.textSize = 40f
+        textView.text = message
+
+        // Set the text view as the activity layout
+        setContentView(textView)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                NavUtils.navigateUpFromSameTask(this)
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
